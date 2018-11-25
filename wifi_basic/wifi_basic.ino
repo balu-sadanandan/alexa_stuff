6cc6dd0755e55ece6fe8aba96f858d4a41a68ecc
Balu <balu.sadanandan.as@gmail.com>
2018-11-25 17:57:59 +0530

    esp

--- wifi_basic/wifi_basic.ino
+++ wifi_basic/wifi_basic.ino
@@ -21,7 +21,7 @@ void checkForWIFI(void)
     Serial.println(WLAN_SSID);
     while (WiFi.status() != WL_CONNECTED)
     {
-       Serial.println(".");
+       Serial.print(".");
       delay(1000);
     }
      Serial.println(" ");
@@ -32,7 +32,7 @@ void checkForWifiStatus(void)
 {
   if (WiFi.status() != WL_CONNECTED)
   {
-  Serial.println("Wait for WIFI ");
+  Serial.println("Waiting for ");
     Serial.println(WLAN_SSID);
     WiFi.reconnect();
     while (WiFi.status() != WL_CONNECTED)
@@ -47,10 +47,15 @@ void checkForWifiStatus(void)
 void configureWIFI(void)
 {
   // WIFI Connection
-   Serial.println("Connecting to ");
-  Serial.println(WLAN_SSID);
+   
   // Connect to WiFi access point.
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_OFF);   // this is a temporary line, to be removed after SDK update to 1.5.4
+    WiFi.mode(WIFI_STA);
+    Serial.println("Connecting to ");
+    Serial.println(WLAN_SSID);
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+  
   checkForWIFI();
   delay(100);
 }
@@ -60,23 +65,7 @@ void setup()
     delay(10);
      configureWIFI();
     // We start by connecting to a WiFi network
-   /* WiFiMulti.addAP(WLAN_SSID,WLAN_PASS);
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Wait for WiFi... ");
-
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    delay(500);*/
+   
 }
 
 
@@ -86,31 +75,5 @@ void loop()
     const char * host = "192.168.1.1"; // ip or dns
 
   checkForWifiStatus();
-/*
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-
-    if (!client.connect(host, port)) {
-        Serial.println("connection failed");
-        Serial.println("wait 5 sec...");
-        delay(5000);
-
-        return;
-    }
-
-    // This will send the request to the server
-    client.print("Send this data to server");
-
-    //read back one line from server
-    String line = client.readStringUntil('\r');
-    client.println(line);
-
-    Serial.println("closing connection");
-    client.stop();
-
-    Serial.println("wait 5 sec...");
-    delay(5000);*/
+delay(1000);
 }
